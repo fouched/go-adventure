444c07ad3c5784e096fe1474b540e293c0ad3489
Fouche du Preez <fouche@limehouse.co.za>
2024-08-17 22:20:37 +0200

    fixed welcome text

--- cmd/game.go
+++ cmd/game.go
@@ -38,7 +38,7 @@ func welcome() {
     The village of Honeywood has been terrorized by strange, deadly creatures for months now. Unable to endure any 
     longer, the villagers pooled their wealth and hired the most skilled adventurer they could find: you. After
     listening to their tale of woe, you agree to enter the labyrinth where most of the creatures seem to originate,
-    and destroy the foul beasts. Armed with a longsword and a bundle of torches, you descend into the labyrinth, 
+    and destroy the foul beasts. Armed with nothing but a bundle of torches, you descend into the labyrinth, 
     ready to do battle....
 	`)
 }
